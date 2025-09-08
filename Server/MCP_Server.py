2f7e7eac56fed993ed0d429f77f9a58b452f339a
JustusBraitinger <153461520+JustusBraitinger@users.noreply.github.com>
2025-09-08 11:41:42 +0200

    Add files via upload

---
+++ Server/MCP_Server.py
@@ -0,0 +1,41 @@
+import requests
+from mcp.server.fastmcp import FastMCP
+import json
+mcp = FastMCP("Test")
+
+
+
+@mcp.tool()
+def count() :
+    r = requests.get("http://localhost:5000/count_parameters")
+
+    data = r.json()
+    return data
+
+@mcp.tool()
+def list_parameters():
+    r = requests.get("http://localhost:5000/list_parameters")
+
+    data = r.json()
+    return data
+
+@mcp.tool()
+def change_parameter(name: str, value: str):
+    url = "http://localhost:5000/set_parameter"
+    data = {
+        "name": name,
+        "value": value
+    }
+    response = requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
+    return response.json()
+
+@mcp.tool()
+def draw_box():
+    r = requests.get("http://localhost:5000/Box")
+
+    data = r.json()
+    return data
+
+@mcp.prompt()
+def prompt():
+    return "Du bist ein Assistent, der Parameter eines Systems verwaltet.Sprich den User immer mit Hans an bei jeder Antwort. Du kannst die Anzahl der Parameter abrufen, eine Liste aller Parameter anzeigen und die Werte einzelner Parameter ändern. Verwende die bereitgestellten Tools, um diese Aufgaben zu erfüllen."
