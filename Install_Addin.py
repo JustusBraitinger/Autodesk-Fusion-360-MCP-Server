e839a07a98649ebdecf1c0104a578c45f36c6ce1
JustusBraitinger <153461520+JustusBraitinger@users.noreply.github.com>
2025-12-08 08:39:39 +0100

    Add files via upload

---
+++ Install_Addin.py
@@ -0,0 +1,23 @@
+import os 
+import shutil 
+
+
+userdir = os.path.expanduser('~')
+addin_path = os.path.join(userdir, "AppData", "Roaming", "Autodesk", "Autodesk Fusion 360", "API", "AddIns")
+source_folder = os.path.join(userdir,"FusionMCP","MCP")
+
+print(f"Getting the folder from {source_folder}")
+print(f"Installing the add-in to {addin_path}")
+
+
+name = os.path.basename(source_folder)
+destination_folder = os.path.join(addin_path, name)
+
+os.makedirs(destination_folder, exist_ok=True)
+shutil.copytree(source_folder, destination_folder, dirs_exist_ok=True)
+
+exist = os.path.exists(destination_folder)
+if not exist:
+    raise Exception(f"Add-in installation failed.\n Please look into the README for manual installation instructions.\n It is important, that you have the repo in the userdir {userdir}\\FusionMCP")
+else:
+    print("Add-in installed successfully.")
